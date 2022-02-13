626f95360261b76774b91b8741575f42805b894b
18chanp1 <18chanp1@gmail.com>
2022-02-12 18:31:00 -0800

    task2 prelim, untested

--- task2/cpen212alloc.c
+++ task2/cpen212alloc.c
@@ -5,12 +5,18 @@
 typedef struct {
     void *end;   // end of heap
     void *free;  // next free address on heap
+    void *head; //head of the heap
 } alloc_state_t;
 
 void *cpen212_init(void *heap_start, void *heap_end) {
     alloc_state_t *s = (alloc_state_t *) malloc(sizeof(alloc_state_t));
     s->end = heap_end;
     s->free = heap_start;
+    s->head = heap_start;
+
+    //header block
+    *(unsigned long long int *)s->head = heap_end - heap_start;  // store int value 10 at address 0x16
+    
     return s;
 }
 
@@ -21,20 +27,67 @@ void cpen212_deinit(void *s) {
 void *cpen212_alloc(void *alloc_state, size_t nbytes) {
     alloc_state_t *s = (alloc_state_t *) alloc_state;
     size_t aligned_sz = (nbytes + 7) & ~7;
-    void *p = s->free;
-    if ((p >= s->end) || (p + aligned_sz > s->end))
+
+    unsigned long long int *currentBlock = s->head;
+
+    while((*currentBlock & 1) && (currentBlock + 8 < s->end) && 
+            *currentBlock > aligned_sz){
+        currentBlock = currentBlock + *currentBlock;
+    }
+
+    if(currentBlock + 8 >= s->end || 
+        currentBlock + aligned_sz + 8 > s ->end 
+        || nbytes < 0){
         return NULL;
-    s->free += aligned_sz;
-    return p;
+    }
+
+    void *memblock = currentBlock + 8;
+
+    *(unsigned long long int *)currentBlock = aligned_sz + 8 + 1;
+
+    return currentBlock + 8;
+
+
+
+    // void *p = s->free;
+    // if ((p >= s->end) || (p + aligned_sz > s->end))
+    //     return NULL;
+    // s->free += aligned_sz;
+    // return currentBlock + 8;
 }
 
-void cpen212_free(void *s, void *p) { }
+void cpen212_free(void *alloc_state, void *p) {
+    unsigned long long *currentblock = p - 8;
+    *currentblock--;
+ }
 
-void *cpen212_realloc(void *s, void *prev, size_t nbytes) {
-    void *p = cpen212_alloc(s, nbytes);
-    if (p != NULL)
-        memmove(p, prev, nbytes); // see WARNING below
-    return p;
+void *cpen212_realloc(void *alloc_state, void *prev, size_t nbytes) {
+    //get header
+    size_t aligned_sz = (nbytes + 7) & ~7;
+    unsigned long long* oldBlock = prev-8;
+    if (*oldBlock == aligned_sz){
+        return prev;
+    }
+    if (*oldBlock > aligned_sz) {
+        unsigned long long *splitBlock = oldBlock + 8 + aligned_sz;
+        *(unsigned long long int *)splitBlock = *oldBlock - aligned_sz - 8;
+
+        *(unsigned long long int *)oldBlock = aligned_sz + 1 + 8;
+
+        return prev;
+    }
+    if (*oldBlock > aligned_sz){
+        void * newPtr = cpen212_alloc(alloc_state, aligned_sz);
+        memmove(newPtr, prev, *oldBlock - 8);
+    }
+
+
+
+
+    // void *p = cpen212_alloc(s, nbytes);
+    // if (p != NULL)
+    //     memmove(p, prev, nbytes); // see WARNING below
+    // return p;
 }
 
 // WARNING: we don't know the prev block's size, so memmove just copies nbytes here.
